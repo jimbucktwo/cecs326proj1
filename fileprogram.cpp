4e583ce7350b973188e9d16ca3be5e8fc959929c
jimbucktwo <92623786+jimbucktwo@users.noreply.github.com>
2024-02-17 16:46:29 -0800

    update

---
+++ fileprogram.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+   // printf() displays the string inside quotation
+   cout << "Hello World!";
+   return 0;
+}
